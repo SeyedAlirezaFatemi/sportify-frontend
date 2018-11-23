88cc8363cdc85ade1dcd49c536cbb70b59d17fd9
Alireza <alirezafa@rocketmail.com>
2018-11-23 13:09:56 +0330

    added some components

---
+++ src/components/ProfileCard/ProfileCard.tsx
@@ -0,0 +1,22 @@
+import { Avatar, Card, Icon } from 'antd';
+import * as React from 'react';
+
+const { Meta } = Card;
+
+const ProfileCard: React.FunctionComponent = () => (
+  <Card
+    style={{ width: 300 }}
+    cover={<img alt="example" src="https://gw.alipayobjects.com/zos/rmsportal/JiqGstEfoWAOHiTxclqi.png" />}
+    actions={[<Icon key="setting" type="setting" />, <Icon key="edit" type="edit" />,
+      <Icon key="ellipsis" type="ellipsis" />]}
+  >
+    <Meta
+      avatar={<Avatar src="https://zos.alipayobjects.com/rmsportal/ODTLcjxAfvqbxHnVXCYX.png" />}
+      title="Card title"
+      description="This is the description"
+    />
+  </Card>
+);
+
+
+export default ProfileCard;
---
+++ src/components/index.ts
@@ -0,0 +1,10 @@
+import EventTimeline from './EventTimeline/EventTimeline'
+import NewsList from './NewsList/NewsList'
+import ProfileCard from './ProfileCard/ProfileCard'
+
+
+export {
+  ProfileCard,
+  NewsList,
+  EventTimeline
+}
