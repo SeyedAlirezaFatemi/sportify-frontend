16cc55dcd0a3c365419e180b6884e11b92e18b53
Alireza <alirezafa@rocketmail.com>
2018-11-25 12:36:53 +0330

    tabbed

--- src/components/NewsList/NewsList.tsx
+++ src/components/NewsList/NewsList.tsx
@@ -1,4 +1,4 @@
-import { Avatar, Icon, List } from 'antd';
+import { Avatar, Icon, List, Tabs } from 'antd';
 import * as React from 'react';
 
 const listData: any[] = [];
@@ -19,34 +19,43 @@ const IconText = ({ type, text }) => (
   </span>
 );
 
+const TabPane = Tabs.TabPane;
+
 const NewsList: React.FunctionComponent = () => (
-  <List
-    itemLayout="vertical"
-    size="large"
-    pagination={{
-      onChange: (page) => {
-        console.log(page);
-      },
-      pageSize: 3,
-    }}
-    dataSource={listData}
-    footer={<div><b>ant design</b> footer part</div>}
-    renderItem={item => (
-      <List.Item
-        key={item.title}
-        actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />,
-          <IconText type="message" text="2" />]}
-        extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
-      >
-        <List.Item.Meta
-          avatar={<Avatar src={item.avatar} />}
-          title={<a href={item.href}>{item.title}</a>}
-          description={item.description}
-        />
-        {item.content}
-      </List.Item>
-    )}
-  />
+  <Tabs defaultActiveKey="1">
+    <TabPane tab="All" key="1">
+      <List
+        itemLayout="vertical"
+        size="large"
+        pagination={{
+          onChange: (page) => {
+            console.log(page);
+          },
+          pageSize: 3,
+        }}
+        dataSource={listData}
+        footer={<div><b>ant design</b> footer part</div>}
+        renderItem={item => (
+          <List.Item
+            key={item.title}
+            actions={[<IconText type="star-o" text="156" />, <IconText type="like-o" text="156" />,
+              <IconText type="message" text="2" />]}
+            extra={<img width={272} alt="logo" src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
+          >
+            <List.Item.Meta
+              avatar={<Avatar src={item.avatar} />}
+              title={<a href={item.href}>{item.title}</a>}
+              description={item.description}
+            />
+            {item.content}
+          </List.Item>
+        )}
+      />
+    </TabPane>
+    <TabPane tab="Subscribed" disabled key="2">
+      {/*If user is logged in*/}
+    </TabPane>
+  </Tabs>
 );
 
 export default NewsList;
