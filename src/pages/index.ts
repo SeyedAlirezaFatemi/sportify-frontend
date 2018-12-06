eb788e846971fbe2a290ccf33032fe77e9f57632
amirHosseinS3 <ahns3.ahe@gmail.com>
2018-12-06 19:50:10 +0330

    Fix minor bugs

--- src/pages/index.ts
+++ src/pages/index.ts
@@ -2,10 +2,12 @@ import HomePage from './HomePage/HomePage';
 import NewsPage from './NewsPage/NewsPage';
 import TeamPage from './TeamPage/TeamPage';
 import Template from './Template/Template';
+import LeaguePage from './LeaguePage/LeaguePage';
 
 export {
   HomePage,
   Template,
   TeamPage,
   NewsPage,
+  LeaguePage,
 }
