f10fe4ddb6d892e7ae51fb098833c9b6b54c3bf7
Ali Heydari <ali4heydari@gmail.com>
2018-12-06 23:11:47 +0330

    Add semantic-UI package

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import './App.css';
-import TeamPage from "./pages/TeamPage/TeamPage";
+import GamePage from "./pages/GamePage/GamePage";
 
 class App extends React.Component {
   public render() {
-    return (<TeamPage />);
+    return (<GamePage />);
   }
 }
 
