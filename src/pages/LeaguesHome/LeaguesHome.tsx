89ae325e4a630b815ce57e85ef1ceae25101a269
amirHosseinS3 <ahns3.ahe@gmail.com>
2018-12-10 14:51:47 +0330

    Leagues Page

--- src/pages/LeaguesHome/LeaguesHome.tsx
+++ src/pages/LeaguesHome/LeaguesHome.tsx
@@ -1,8 +1,11 @@
-import { Card, Input, Tabs } from 'antd';
+import {Card, Input, Tabs} from 'antd';
 import * as React from "react";
-import { Component } from "react";
-import { Template } from "../";
+import {Component} from "react";
+import {Template} from "../";
 import './LeaugesHome.css';
+import Row from "antd/es/grid/row";
+import Col from "antd/es/grid/col";
+import GamesList from "../../components/GameComponents/GamesList/GamesList";
 
 
 const Search = Input.Search;
@@ -17,6 +20,27 @@ const basketballLeague = [
   'WNBA',
 ];
 
+const footballLeagueArchives = [{
+  title: 'Premier League Archive',
+  years: ['2017', '2016', '2015'],
+}, {
+  title: 'Laliga League Archive',
+  years: ['2017', '2016', '2015'],
+}, {
+  title: 'Serie A Archive',
+  years: ['2017', '2016', '2015'],
+}
+];
+
+const basketballLeagueArchives = [{
+  title: 'NBA',
+  years: ['2017', '2016', '2015'],
+}, {
+  title: 'WNBA',
+  years: ['2017', '2016', '2015'],
+},
+];
+
 
 class LeaguesHome extends Component {
   public render(): React.ReactNode {
@@ -26,38 +50,39 @@ class LeaguesHome extends Component {
 
     return (
       <Template>
-        <Search
-          placeholder="search league"
-          onSearch={value => console.log(value)}
-          style={{ width: 200 }}
-        />
-        <Tabs defaultActiveKey="1" onChange={callback}>
-          <TabPane tab="Current Leagues" key="1">
-            <Card
-              title="Football"
-              style={{ width: 300 }}
-            >
-              <p>Premier League</p>
-              <p>Laliga League</p>
-            </Card>
-            <Card
-              title="Basketball"
-              style={{ width: 300 }}
-            >
-              <p>NBA League</p>
-              <p>WNBA League</p>
-            </Card>
-          </TabPane>
-          <TabPane tab="Old Leagues" key="2">
-            <Card
-              title="Premier League Archive"
-              style={{ width: 300 }}
-            >
-              <p></p>
-            </Card>
-          </TabPane>
-        </Tabs>
-      </Template>
+        <GamesList/>
+      {/*<Search*/}
+        {/*placeholder="search league"*/}
+        {/*onSearch={value => console.log(value)}*/}
+        {/*className="search-box"*/}
+      {/*/>*/}
+      {/*<Tabs defaultActiveKey="1" onChange={callback}>*/}
+        {/*<TabPane tab="Current Leagues" key="1">*/}
+              {/*<Card*/}
+                {/*title="Football"*/}
+                {/*className="league-table"*/}
+              {/*>*/}
+                {/*{footballLeague.map((league) => <div><a>{league}</a> <br/></div>)}*/}
+              {/*</Card>*/}
+              {/*<Card*/}
+                {/*title="Basketball"*/}
+                {/*className="league-table"*/}
+              {/*>*/}
+                {/*{basketballLeague.map((league) => <div><a>{league}</a> <br/></div>)}*/}
+              {/*</Card>*/}
+        {/*</TabPane>*/}
+
+        {/*<TabPane tab="Old Leagues" key="2">*/}
+          {/*{footballLeagueArchives.map((league) => <Card className="league-table" title={league.title}>*/}
+            {/*{league.years.map((years) => <div><a>{years}</a> <br/></div>)}*/}
+          {/*</Card>)}*/}
+          {/*{basketballLeagueArchives.map((league) => <Card className="league-table" title={league.title}>*/}
+            {/*{league.years.map((years) => <div><a>{years}</a> <br/></div>)}*/}
+          {/*</Card>)}*/}
+        {/*</TabPane>*/}
+
+      {/*</Tabs>*/}
+    </Template>
     )
   }
 }
