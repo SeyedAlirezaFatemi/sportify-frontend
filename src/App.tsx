0fbc4a06686bd44022ab545bbe14ee7120b95dca
amirHosseinS3 <ahns3.ahe@gmail.com>
2018-12-11 16:21:18 +0330

    League home page beautified :)

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
 import './App.css';
 import { GamePage, HomePage, LeagueHomePage, NewsPage, NotFoundPage, PlayerPage, TeamPage } from './pages';
+import LeaguePage from "./pages/LeaguePage/LeaguePage";
 
 class App extends React.Component {
   public render() {
@@ -10,7 +11,8 @@ class App extends React.Component {
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route path="/team/:id" component={TeamPage} />
-          <Route path="/league" component={LeagueHomePage} />
+          <Route exact path="/league" component={LeagueHomePage} />
+          <Route path="/league/:id" component={LeaguePage} />
           <Route path="/player" component={PlayerPage} />
           <Route path="/news/:id" component={NewsPage} />
           <Route path="/game" component={GamePage} />
--- src/pages/LeagueHomePage/LeagueHomePage.tsx
+++ src/pages/LeagueHomePage/LeagueHomePage.tsx
@@ -1,5 +1,5 @@
 import { Grid } from '@material-ui/core';
-import { Card, Input, Tabs } from 'antd';
+import { Table, Divider, Input, Tabs } from 'antd';
 import * as React from 'react';
 import { Component } from 'react';
 import { Template } from '../';
@@ -9,7 +9,103 @@ import './LeaugeHomePage.css';
 const Search = Input.Search;
 const TabPane = Tabs.TabPane;
 
+const leagues = [
+  {
+    key:1,
+    sport: 'Basketball',
+    league: 'NBA',
+    year: 2018,
+    status: 'new',
+    country: 'USA',
+  }, {
+    key:2,
+    sport: 'Basketball',
+    league: 'WNBA',
+    year: 2018,
+    status: 'new',
+    country: 'USA',
+  }, {
+    key:3,
+    sport: 'Football',
+    league: 'Premiere League',
+    year: 2018,
+    status: 'new',
+    country: 'England',
+  }, {
+    key:4,
+    sport: 'Football',
+    league: 'LaLiga',
+    year: 2018,
+    status: 'new',
+    country: 'Spain',
+  }, {
+    key:5,
+    sport: 'Football',
+    league: 'Serie A League',
+    year: 2018,
+    status: 'new',
+    country: 'Italy',
+  }, {
+    key:6,
+    sport: 'Basketball',
+    league: 'NBA',
+    year: 2017,
+    status: 'old',
+    country: 'USA',
+  }, {
+    key:7,
+    sport: 'Basketball',
+    league: 'WNBA',
+    year: 2017,
+    status: 'old',
+    country: 'USA',
+  }, {
+    key:8,
+    sport: 'Football',
+    league: 'Premiere League',
+    year: 2017,
+    status: 'old',
+    country: 'England',
+  }, {
+    key:9,
+    sport: 'Football',
+    league: 'LaLiga',
+    year: 2017,
+    status: 'old',
+    country: 'Spain',
+  }, {
+    key:10,
+    sport: 'Football',
+    league: 'Serie A League',
+    year: 2017,
+    status: 'old',
+    country: 'Italy',
+  },
+];
+
+const columns = [{
+  title: 'League',
+  dataIndex: 'league',
+  key: 'league',
+  render: (text, id) => <a href="/team/1">{text}</a>,
+}, {
+  title: 'Year',
+  dataIndex: 'year',
+  key: 'year',
+}, {
+  title: 'Country',
+  dataIndex: 'country',
+  key: 'country',
+}
+];
 class LeagueHomePage extends Component {
+
+  private leagueFilter = (status, sport) => {
+    return leagues.filter((league) => {
+      return league.sport === sport && league.status === status;
+    });
+  };
+
   public render(): React.ReactNode {
     return (
       <Template>
@@ -22,34 +118,30 @@ class LeagueHomePage extends Component {
         </Grid>
         <Tabs defaultActiveKey="1">
           <TabPane tab="Current Leagues" key="1">
-            <Grid container direction="row" justify="space-evenly">
-              <Grid item>
-                <Card
-                  title="Football"
-                  style={{ width: 300 }}
-                >
-                  <p>Premier League</p>
-                  <p>Laliga League</p>
-                </Card>
-              </Grid>
-              <Grid item>
-                <Card
-                  title="Basketball"
-                  style={{ width: 300 }}
-                >
-                  <p>NBA League</p>
-                  <p>WNBA League</p>
-                </Card>
-              </Grid>
-            </Grid>
+            <Tabs
+              tabPosition="left"
+              defaultActiveKey="1"
+            >
+              <TabPane tab="Football" key="1">
+                <Table columns={columns} dataSource={this.leagueFilter('new', 'Football')} pagination={false} />
+              </TabPane>
+              <TabPane tab="Basketball" key="2">
+                <Table columns={columns} dataSource={this.leagueFilter('new', 'Basketball')} pagination={false} />
+              </TabPane>
+            </Tabs>
           </TabPane>
           <TabPane tab="Old Leagues" key="2">
-            <Card
-              title="Premier League Archive"
-              style={{ width: 300 }}
+            <Tabs
+              tabPosition="left"
+              defaultActiveKey="1"
             >
-              <p></p>
-            </Card>
+              <TabPane tab="Football" key="1">
+                <Table columns={columns} dataSource={this.leagueFilter('old', 'Football')} pagination={false} />
+              </TabPane>
+              <TabPane tab="Basketball" key="2">
+                <Table columns={columns} dataSource={this.leagueFilter('old', 'Basketball')} pagination={false} />
+              </TabPane>
+            </Tabs>
           </TabPane>
         </Tabs>
       </Template>
