ccf107dc0d92386559c86f1d62ab7b907a92dd61
Alireza <alirezafa@rocketmail.com>
2018-12-14 01:44:12 +0330

    better team and player

--- src/components/Player/PlayerHeader/PlayerHeader.tsx
+++ src/components/Player/PlayerHeader/PlayerHeader.tsx
@@ -1,16 +1,18 @@
+import Grid from '@material-ui/core/Grid';
 import { Avatar } from 'antd';
 import * as React from 'react';
 import { PlayerInfo } from '..';
-import './PlayerHeader.css'
 
 
 const PlayerHeader: React.FunctionComponent = () => (
-  <div id={'container-div'}>
-    <div id={'avatar-info-div'}>
+  <Grid container direction="row" justify="center" alignItems="center">
+    <Grid item container justify="center" alignItems="center" xs={12} xl={6} sm={12} md={6} lg={6}>
       <Avatar size={250} icon="user" />
+    </Grid>
+    <Grid item xs={12} xl={6} sm={12} md={6} lg={6}>
       <PlayerInfo />
-    </div>
-  </div>
+    </Grid>
+  </Grid>
 );
 
 export default PlayerHeader;
--- src/components/Team/index.ts
+++ src/components/Team/index.ts
@@ -1,8 +1,10 @@
 import TeamHeader from './TeamHeader/TeamHeader';
+import TeamInfo from './TeamInfo/TeamInfo';
 import TeamPlayersList from './TeamPlayersList/TeamPlayersList';
 
 
 export {
   TeamHeader,
   TeamPlayersList,
+  TeamInfo,
 }
