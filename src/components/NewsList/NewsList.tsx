adcffec964ce006d2b19b8dbdfa1524b380cb97b
Alireza <alirezafa@rocketmail.com>
2019-01-23 12:09:36 +0330

    example axios.

--- src/components/NewsList/NewsList.tsx
+++ src/components/NewsList/NewsList.tsx
@@ -4,17 +4,6 @@ import { withRouter } from 'react-router-dom';
 import axios from '../../api';
 import { API } from '../../utils';
 
-const listData: any[] = [];
-for (let i = 0; i < 23; i++) {
-  listData.push({
-    href: 'http://ant.design',
-    title: `Fatemi S3 part ${i}`,
-    avatar: 'https://d9np3dj86nsu2.cloudfront.net/thumb/0c771449acaecb388c58d8805d966f61/350_403',
-    description: 'A Team ofh strong cool guys',
-    content: 'We supply a series of design principles, practical patterns and high quality design resources (Sketch and Axure), to help people create their product prototypes beautifully and efficiently.',
-  });
-}
-
 const IconText = ({ type, text }) => (
   <span>
     <Icon type={type} style={{ marginRight: 8 }} />
@@ -25,18 +14,19 @@ const IconText = ({ type, text }) => (
 const TabPane = Tabs.TabPane;
 
 class NewsList extends React.Component<any, any> {
-  state = {
-    data: [],
+  public state = {
+    news: [],
   };
 
   public componentDidMount(): void {
     axios.get(API.LATEST_NEWS).then(response => {
-      console.log(response)
+      this.setState({ news: response.data })
     })
   }
 
   public render(): React.ReactNode {
     const { history } = this.props;
+    const { news } = this.state;
     return (
       <div style={{ padding: '8px' }}>
         <Tabs defaultActiveKey="1">
@@ -50,21 +40,21 @@ class NewsList extends React.Component<any, any> {
                 },
                 pageSize: 3,
               }}
-              dataSource={listData}
+              dataSource={news}
               renderItem={item => (
                 <List.Item
-                  key={item.title}
+                  key={item.id}
                   actions={[<IconText type="message" text="2" />
-                    , <Button htmlType="button" onClick={() => history.push('/news/1')}>More</Button>]}
+                    , <Button htmlType="button" onClick={() => history.push(`/news/${item.id}`)}>More</Button>]}
                   extra={<img width={272} alt="logo"
                               src="https://gw.alipayobjects.com/zos/rmsportal/mqaQswcyDLcXyDKnZfES.png" />}
                 >
                   <List.Item.Meta
                     avatar={<Avatar src={item.avatar} />}
-                    title={<a href={item.href}>{item.title}</a>}
-                    description={item.description}
+                    title={<a>{item.title}</a>}
+                    description={item.pub_date}
                   />
-                  {item.content}
+                  {item.brief}
                 </List.Item>
               )}
             />
