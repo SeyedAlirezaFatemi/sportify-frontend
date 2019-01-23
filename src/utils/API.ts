1104ac59c4884560f93bdd12a7c95bbe70e416fe
Alireza <alirezafa@rocketmail.com>
2019-01-23 18:25:34 +0330

    Progress.

--- src/utils/API.ts
+++ src/utils/API.ts
@@ -1,12 +1,20 @@
 export enum API {
+  NEWS_DETAILS = 'news/detail/',
+
   LATEST_NEWS = 'news/latest/',
+
   LATEST_NEWS_BASKETBALL = 'news/latest/basketball/',
   LATEST_NEWS_SOCCER = 'news/latest/soccer/',
-  RELATED_NEWS_SOCCER_GAME = 'game/soccer/related_news/',
-  RELATED_NEWS_BASKETBALL_GAME = 'game/basketball/related_news/',
-  RELATED_NEWS_SOCCER_TEAM = 'team/soccer/related_news/',
-  RELATED_NEWS_BASKETBALL_TEAM = 'team/basketball/related_news/',
-  NEWS_DETAILS = 'news/detail/',
+
+  RELATED_NEWS_PLAYER = 'sport/player/related_news/',
+
+  RELATED_NEWS_SOCCER_TEAM = 'sport/team/soccer/related_news/',
+  RELATED_NEWS_BASKETBALL_TEAM = 'sport/team/basketball/related_news/',
+
 }
 
 export const PlayerInfoAPI = (sport: string, id: number) => `sport/player/${sport}/info/${id}/`;
+
+export const PlayerNewsAPI = (id: number) => `${API.RELATED_NEWS_PLAYER}${id}/`;
+
+
